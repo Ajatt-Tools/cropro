3b4f1bf199737045d3e7368020ce6d9c6ec99fcc
Ren Tatsumoto <tatsu@autistici.org>
2020-12-18 13:31:12 +0300

    update build.sh
    support manifests for sharing outside of ankiweb

--- build.sh
+++ build.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-rm cropro.ankiaddon
-zip -r cropro.ankiaddon ./*.py ./*.json
+
+readonly archive=cropro.ankiaddon
+readonly manifest=manifest.json
+readonly target=${*:-ankiweb}
+
+rm -- $archive 2>/dev/null
+
+if [[ $target != 'ankiweb' ]]; then
+    # https://addon-docs.ankiweb.net/#/sharing?id=sharing-outside-ankiweb
+    # If you wish to distribute .ankiaddon files outside of AnkiWeb,
+    # your add-on folder needs to contain a ‘manifest.json’ file.
+    {
+        echo '{'
+        echo -e '\t"package": "CroPro",'
+        echo -e '\t"name": "Cross Profile Search and Import",'
+        echo -e "\t\"mod\": $(date -u '+%s')"
+        echo '}'
+    } > $manifest
+fi
+
+zip -r $archive -- ./*.py ./*.json
+rm -- $manifest 2>/dev/null
