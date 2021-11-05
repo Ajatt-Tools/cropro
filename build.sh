2cdccf0eb330098b3576aedad08e745a7b72ce4a
Ren Tatsumoto <tatsu@autistici.org>
2021-11-05 16:29:09 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,47 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Copyright: Ren Tatsumoto <tatsu at autistici.org>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-readonly archive=cropro.ankiaddon
+readonly addon_name=ajt_cropro
 readonly manifest=manifest.json
-readonly target=${*:-ankiweb}
+readonly root_dir=$(git rev-parse --show-toplevel)
+readonly branch=${1:-$(git branch --show-current)}
+readonly zip_name=${addon_name}_${branch}.ankiaddon
+export root_dir branch
 
-rm -- "$archive" 2>/dev/null
+ask_target() {
+	local target
+	read -r target
+	echo "$target"
+}
 
-if [[ $target != 'ankiweb' ]]; then
-    # https://addon-docs.ankiweb.net/#/sharing?id=sharing-outside-ankiweb
-    # If you wish to distribute .ankiaddon files outside of AnkiWeb,
-    # your add-on folder needs to contain a ‘manifest.json’ file.
-    {
-        echo '{'
-        echo -e '\t"package": "CroPro",'
-        echo -e '\t"name": "Cross Profile Search and Import",'
-        echo -e "\t\"mod\": $(date -u '+%s')"
-        echo '}'
-    } > $manifest
-fi
+git_archive() {
+	echo -n "Enter target [ankiweb/else]: "
+	if [[ $(ask_target) != 'ankiweb' ]]; then
+		# https://addon-docs.ankiweb.net/sharing.html#sharing-outside-ankiweb
+		# If you wish to distribute .ankiaddon files outside of AnkiWeb,
+		# your add-on folder needs to contain a ‘manifest.json’ file.
+		echo "Creating $manifest"
+		{
+			echo '{'
+			echo -e '\t"package": "CroPro",'
+			echo -e '\t"name": "Cross Profile Search and Import",'
+			echo -e "\t\"mod\": $(date -u '+%s')"
+			echo '}'
+		} >"$manifest"
+		git archive "$branch" --format=zip --output "$zip_name" --add-file="$manifest"
+	else
+		git archive "$branch" --format=zip --output "$zip_name"
+	fi
+}
 
-zip -r $archive -- ./*.py ./*.json
-rm -- $manifest 2>/dev/null
+cd -- "$root_dir" || exit 1
+rm -- ./"$zip_name"
+
+git_archive
+
+# shellcheck disable=SC2016
+git submodule foreach 'git archive HEAD --prefix=$path/ --format=zip --output "$root_dir/${path}_${branch}.zip"'
+
+zipmerge ./"$zip_name" ./*.zip
+rm -- ./*.zip ./"$manifest"
