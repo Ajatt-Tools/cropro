39c54a649da74a38ff7d60e077bd7cd4bdf2a532
Ren Tatsumoto <tatsu@autistici.org>
2024-03-01 22:18:48 +0300

    update config
    add timeout settings, add properties

--- config.md
+++ config.md
@@ -5,10 +5,10 @@
 ### List of options:
 
 * `enable_debug_log` - print debug information to `stdout` and to a log file.
-Log location: `~/.local/share/Anki2/cropro.log` (GNU systems).
+   Log location: `~/.local/share/Anki2/cropro.log` (GNU systems).
 * `max_displayed_notes` - how many search result to display
-* `tag_original_notes` - tag cards in the other profile as `exported`
-so that you could easily find and delete them later.
+* `exported_tag` - tag that is added to cards in the other profile
+   so that you could easily find and delete them later.
 * `hidden_fields` - contents of fields that contain these keywords won't be shown.
 * `allow_empty_search` - Search notes even if the search field is emtpy. May be slow.
 * `call_add_cards_hook` - Calls the `add_cards_did_add_note` hook as soon as a note
--- config.py
+++ config.py
@@ -7,29 +7,105 @@ from .ajt_common.addon_config import AddonConfigManager
 
 
 class CroProConfig(AddonConfigManager):
+    @property
     def tag_original_notes(self) -> Optional[str]:
         """
         Tag that is added to original notes (in the other profile)
         to mark that they have been copied to the current profile.
         """
-        if self['tag_original_notes'] and (tag := self['exported_tag']):
-            return tag
+        return self["exported_tag"].strip()
+
+    @tag_original_notes.setter
+    def tag_original_notes(self, new_value: str) -> None:
+        self["exported_tag"] = new_value.strip()
 
     @property
     def allow_empty_search(self) -> bool:
+        """
+        Perform search with empty search bar.
+        Could be used to transfer all notes at once, although not really practical.
+        """
         return self["allow_empty_search"]
 
     @property
     def search_the_web(self) -> bool:
+        """
+        Whether to search the web or a local collection.
+        """
         return self["search_the_web"]
 
     @search_the_web.setter
-    def search_the_web(self, value: bool):
+    def search_the_web(self, value: bool) -> None:
         self["search_the_web"] = bool(value)
 
     @property
     def max_displayed_notes(self) -> int:
+        """
+        The note list will not show more notes than this value.
+        """
         return self["max_displayed_notes"]
+
+    @max_displayed_notes.setter
+    def max_displayed_notes(self, new_value: int) -> None:
+        self["max_displayed_notes"] = new_value
+
+    @property
+    def timeout_seconds(self) -> int:
+        """
+        Give up trying to connect to the remote server after this many seconds.
+        """
+        return self["timeout_seconds"]
+
+    @timeout_seconds.setter
+    def timeout_seconds(self, timeout: int) -> None:
+        self["timeout_seconds"] = timeout
+
+    @property
+    def hidden_fields(self) -> list[str]:
+        """
+        A list of fields that won't be displayed in the note list.
+        """
+        return self["hidden_fields"]
+
+    @hidden_fields.setter
+    def hidden_fields(self, new_value: list[str]) -> None:
+        self["hidden_fields"] = new_value
+
+    @property
+    def preview_on_right_side(self) -> bool:
+        """
+        Whether to preview notes or not.
+        """
+        return self["preview_on_right_side"]
+
+    @property
+    def enable_debug_log(self) -> bool:
+        return self["enable_debug_log"]
+
+    @property
+    def copy_card_data(self) -> bool:
+        """
+        Copy review-related info, such as due date and interval.
+        """
+        return self["copy_card_data"]
+
+    @property
+    def copy_tags(self) -> bool:
+        return self["copy_tags"]
+
+    @property
+    def skip_duplicates(self) -> bool:
+        """
+        If a note is a duplicate, it won't be imported.
+        """
+        return self["skip_duplicates"]
+
+    @property
+    def call_add_cards_hook(self) -> bool:
+        """
+        Call the `add_cards_did_add_note` hook after a note is imported.
+        """
+        return self["call_add_cards_hook"]
 
 
 config = CroProConfig()
