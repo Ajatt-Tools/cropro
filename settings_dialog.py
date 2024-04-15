8f3da41ed3b01bcd17caf549a7aa10ec0216ae40
Ren Tatsumoto <tatsu@autistici.org>
2024-04-15 19:26:39 +0300

    restore original name

--- settings_dialog.py
+++ settings_dialog.py
@@ -30,7 +30,7 @@ class CroProSettingsDialog(QDialog):
         self.tab_view = QTabWidget()
         self.checkboxes = make_checkboxes()
         self.tag_edit = QLineEdit(config.exported_tag)
-        self.npp_edit = CroProSpinBox(min_val=10, max_val=10_000, step=50, value=config.max_displayed_notes)
+        self.max_notes_edit = CroProSpinBox(min_val=10, max_val=10_000, step=50, value=config.max_displayed_notes)
         self.hidden_fields = ItemEditBox("Hidden fields", initial_values=config.hidden_fields)
         self.web_timeout_spinbox = CroProSpinBox(min_val=1, max_val=999, step=1, value=config.timeout_seconds)
         self.button_box = QDialogButtonBox(
@@ -53,7 +53,7 @@ class CroProSettingsDialog(QDialog):
     def _make_general_tab(self) -> QWidget:
         widget = QWidget()
         layout = QFormLayout()
-        layout.addRow("Notes per page", self.npp_edit)
+        layout.addRow("Max displayed notes", self.max_notes_edit)
         layout.addRow(self.hidden_fields)
         layout.addRow(self.checkboxes["skip_duplicates"])
         layout.addRow(self.checkboxes["copy_tags"])
@@ -188,7 +188,7 @@ class CroProSettingsDialog(QDialog):
         return super().done(result)
 
     def accept(self) -> None:
-        config.max_displayed_notes = self.npp_edit.value()
+        config.max_displayed_notes = self.max_notes_edit.value()
         config.exported_tag = self.tag_edit.text()
         config.hidden_fields = self.hidden_fields.values()
         config.timeout_seconds = self.web_timeout_spinbox.value()
