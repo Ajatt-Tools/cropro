ecbcafc6907db80c2fd97d5bd98daa288f2e387b
Ren Tatsumoto <tatsu@autistici.org>
2024-04-15 17:13:50 +0300

    temp remove

--- __init__.py
+++ __init__.py
@@ -1,11 +1,6 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-# Roadmap:
-# - Improve upper part of search window (more options(?), fast switch for online, collapsible(?))
-# - Improve online request (to-card-converter, importer)
-# - Update search UI to immersion kit like style and add option to merge local and online cards into one search
-
 from . import cropro, settings_dialog
 
 cropro.init()
