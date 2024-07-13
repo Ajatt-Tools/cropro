6cb40cb79a3378cb5ce33ab1b946d3f239ea1b94
Ren Tatsumoto <tatsu@autistici.org>
2024-07-13 06:51:48 +0300

    refactor search result label

--- widgets/search_result_label.py
+++ widgets/search_result_label.py
@@ -1,6 +1,6 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-
+import enum
 import typing
 
 import requests
@@ -13,6 +13,13 @@ class CropProExceptionProtocol(typing.Protocol):
     def what(self) -> str: ...
 
 
+@enum.unique
+class CroProSearchResult(enum.Enum):
+    ok = "green"
+    warn = "orange"
+    error = "red"
+
+
 class SearchResultLabel(QLabel):
     def __init__(self, *args) -> None:
         super().__init__(*args)
@@ -23,33 +30,29 @@ class SearchResultLabel(QLabel):
         self.setStyleSheet("")
         self.hide()
 
+    def _set_status_text(self, text: str, status: CroProSearchResult):
+        self.setText(text)
+        self.setStyleSheet("QLabel { color: %s; }" % status.value)
+        if self.isHidden():
+            self.show()
+
     def set_error(self, ex: CropProExceptionProtocol) -> None:
         text = f"Error: {ex.what()}."
         if ex.response:
             text += f" Status code: {ex.response.status_code}"
-        self.setText(text)
-        self.setStyleSheet("QLabel { color: red; }")
-        if self.isHidden():
-            self.show()
+        self._set_status_text(text, CroProSearchResult.error)
 
     def set_nothing_to_do(self) -> None:
-        self.setText("Search query is empty. Did nothing.")
-        self.setStyleSheet("QLabel { color: orange; }")
-        if self.isHidden():
-            self.show()
+        self._set_status_text("Search query is empty. Did nothing.", CroProSearchResult.warn)
 
     def set_count(self, found_notes: int, displayed_notes: int, current_page_n: int, total_pages_count: int) -> None:
         if found_notes == 0:
-            self.setText("No notes found")
-            self.setStyleSheet("QLabel { color: red; }")
+            return self._set_status_text("No notes found", CroProSearchResult.error)
         elif displayed_notes >= found_notes:
-            self.setText(f"{found_notes} notes found")
-            self.setStyleSheet("QLabel { color: green; }")
+            return self._set_status_text(f"{found_notes} notes found.", CroProSearchResult.ok)
         else:
-            self.setText(
+            return self._set_status_text(
                 f"Displaying {displayed_notes} notes out of {found_notes} total. "
-                f"Page {current_page_n}/{total_pages_count}."
+                f"Page {current_page_n}/{total_pages_count}.",
+                CroProSearchResult.warn,
             )
-            self.setStyleSheet("QLabel { color: orange; }")
-        if self.isHidden():
-            self.show()
